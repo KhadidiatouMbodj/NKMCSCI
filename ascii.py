1d3f80fc3b9ad8880f9c1b7e787851830a29aba5
KhadidiatouMbodj <164446231+KhadidiatouMbodj@users.noreply.github.com>
2024-10-25 12:37:18 -0400

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,6 +1,3 @@
-#name: Ndeye Khadidiatou Mbodj
-#email: ndeyekhadidiatou.mbodj48@myhunter.cuny.edu
-#date: February 12, 2024
 #this program prints ASCII code.
 mango=input('Enter message here:')
 for i in mango:
