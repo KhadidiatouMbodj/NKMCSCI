54ae0f39464c5543732e5d91f38fd847f3b4a256
KhadidiatouMbodj <164446231+KhadidiatouMbodj@users.noreply.github.com>
2024-10-25 12:36:26 -0400

    Update ILP.py

--- ILP.py
+++ ILP.py
@@ -1,6 +1,3 @@
-#name: Ndeye Khadidiatou Mbodj
-#email: ndeyekhadidiatou.mbodj48@myhunter.cuny.edu
-#date: February 12, 2024
 #this program prints ("I love python!") 25 times
 for i in range(25):
     print("I love Python!")
