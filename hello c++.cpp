f90887153fc3e3ba4eaa46fd0f7515994e8ebcf6
KhadidiatouMbodj <164446231+KhadidiatouMbodj@users.noreply.github.com>
2024-10-25 12:42:32 -0400

    Update hello c++.cpp

--- hello c++.cpp
+++ hello c++.cpp
@@ -1,6 +1,3 @@
-//Name:  Ndeye Khadidiatou Mbodj
-//Email:  NDEYEKHADIDIATOU.mbodj48@myhunter.cuny.edu
-//Date:  04/22/2024
 //My first program in C++
 
 #include <iostream>        //The built-in library for input/output
